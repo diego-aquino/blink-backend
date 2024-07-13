11a4e7cb037ef3a4890c1c66e0b205e5bf59ac42
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-07-13 13:58:12 -0300

    feat: configure demo `index.html`

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,14 @@ const PORT = Number(process.env.PORT ?? '3000');
 
 const server = express();
 
-const rootDirectory = path.join(__dirname, '..');
-const publicDirectory = path.join(rootDirectory, 'public');
-server.use(express.static(publicDirectory));
-
 server.get('/', (_request, response) => {
   response.send('Hello World!');
 });
 
+const rootDirectory = path.join(__dirname, '..');
+const publicDirectory = path.join(rootDirectory, 'public');
+server.use(express.static(publicDirectory));
+
 server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server is running at ${HOSTNAME}:${PORT}`);
+  console.log(`Server is running at http://${HOSTNAME}:${PORT}`);
 });
