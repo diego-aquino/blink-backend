efb6f49253f0f6af7acd5259c84e8d1bde26be98
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-07-17 15:24:36 -0300

    chore: configure gulp

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,18 @@ import path from 'path';
 const HOSTNAME = process.env.HOSTNAME ?? '0.0.0.0';
 const PORT = Number(process.env.PORT ?? '3000');
 
-const server = express();
+const app = express();
 
-server.get('/', (_request, response) => {
+app.use(express.json());
+
+app.get('/', (_request, response) => {
   response.send('Hello World!');
 });
 
 const rootDirectory = path.join(__dirname, '..');
 const publicDirectory = path.join(rootDirectory, 'public');
-server.use(express.static(publicDirectory));
+app.use(express.static(publicDirectory));
 
-server.listen(PORT, HOSTNAME, () => {
+app.listen(PORT, HOSTNAME, () => {
   console.log(`Server is running at http://${HOSTNAME}:${PORT}`);
 });
