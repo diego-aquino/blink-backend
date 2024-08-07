1dea47c4e5dc50f6b6a8d70ec1bba63a230ec254
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-08-07 10:15:39 -0300

    feat: add cors

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import userRouter from '../modules/users/router';
 import deprecatedRouter from '../modules/deprecated/router';
 import handleUncaughtError from '../errors/handler';
 
 const app = express();
+
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 
 const rootDirectory = path.join(__dirname, '..');
 const publicDirectory = path.join(rootDirectory, 'public');
