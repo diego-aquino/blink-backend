523ae37fe29d08e009f6c1e703957aceb2ec370a
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-09-02 09:05:07 -0300

    refactor: create schema by worker instead of test file

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,21 +1,23 @@
 import 'express-async-errors';
 
-import { afterAll, beforeAll } from 'vitest';
+import { afterAll, beforeAll, expect } from 'vitest';
 
 import environment from '@/config/environment';
-import { clearDatabase, generateSchemaName } from './utils/database';
+import { clearDatabase, generateTestSchemaName } from './utils/database';
 import database from '@/database/client';
 
-let testSchemaName: string;
+const VITEST_WORKER_ID = process.env.VITEST_WORKER_ID!;
+expect(VITEST_WORKER_ID).toMatch(/^\d+$/);
 
-beforeAll(async (context) => {
-  const testFilePath = context.name;
-  testSchemaName = generateSchemaName(testFilePath);
+const testSchemaName = generateTestSchemaName(VITEST_WORKER_ID);
 
+beforeAll(async () => {
   const testDatabaseURL = environment.DATABASE_URL.replace('schema=public', `schema=${testSchemaName}`);
+  const isTestSchemaPrepared = environment.DATABASE_URL === testDatabaseURL;
 
-  environment.DATABASE_URL = testDatabaseURL;
-  process.env.DATABASE_URL = testDatabaseURL;
+  if (isTestSchemaPrepared) {
+    return;
+  }
 
   database.initialize(testDatabaseURL);
 
@@ -25,6 +27,9 @@ beforeAll(async (context) => {
   const { execa: $ } = await import('execa');
 
   await $`npx dotenv -v DATABASE_URL=${testDatabaseURL} -- npm --silent run migration:apply --skip-generate`;
+
+  process.env.DATABASE_URL = testDatabaseURL;
+  environment.DATABASE_URL = testDatabaseURL;
 });
 
 afterAll(async () => {
